f9ee1b3ed0c9650099c46fff4b96c6de947680ad
Chris <93093030+xChris777@users.noreply.github.com>
2026-02-15 11:10:50 -0500

    Transfer Woodcutting skill to method. Utilize method.

--- tes-grind.rb
+++ tes-grind.rb
@@ -65,7 +65,27 @@ def dialogue_pause(delay_time)
 end
 
 def main_menu
+  loop do
+    
+    puts "
+    Main Menu
+      1. Woodcutting
+      2. Mining
+      3. Exit
+    "
+    selection = gets.chomp.strip
   
+    if selection == "1" || selection == "1."
+      woodcutting_skill
+    elsif selection == "2" || selection == "2."
+        puts "Mining skill coming soon!"
+    elsif selection == "3" || selection == "3."
+        puts "Goodbye!"
+        dialogue_pause(@text_delay)
+        exit
+    else puts "Please input a numerical menu option."
+    end
+end
 end
 
 
@@ -74,7 +94,36 @@ def skill_menu
 end
 
 def woodcutting_skill
-  
+  puts "Press Enter to return to the main menu"
+      
+  puts "You begin to chop a tree"
+  loop do
+    puts "Chop chop chop.."
+    @woodcuttingXP += 2 
+    @charXP += 5 
+    @wood += 3
+    #woodcutting level up system
+    if @woodcuttingXP >= 100
+      @woodcuttingLevel += 1
+      puts "You have reached level #{@woodcuttingLevel}!"
+      @woodcuttingXP = 0
+    end
+    #character level up system
+    if @charXP >= 1000
+      @charLevel += 1
+      puts "You have reached level #{@charLevel}!"
+      @charXP = 0
+    end
+
+    sleep(1)
+    if IO.select([STDIN], nil, nil, 0)
+      STDIN.gets
+      puts "You have #{@wood} pieces of wood"
+      puts "You have achieved level #{@woodcuttingLevel} woodcutting"
+      puts "You have #{@charXP} experience points"
+      break
+    end
+  end 
 end
 
 def mining_skill
@@ -88,8 +137,8 @@ puts "Welcome to TES Grind, an unofficial The Elder Scrolls experience!"
 puts "Please enter your name: "
 
 #sets text delay variables
-text_delay = 2
-text_delay_slow = 3
+@text_delay = 2
+@text_delay_slow = 3
 
 dev_mode = false
 
@@ -99,11 +148,18 @@ name = gets.chomp.capitalize
 if name == "Dev"
   dev_mode = true
   puts "Dev mode has been activated."
-  dialogue_pause(text_delay)
-  puts "Player Name:"
+  puts "Name?"
   name = gets.chomp.capitalize.strip
-  text_delay = 0.2
+  @text_delay = 0.2
+elsif name == "Dev2"
+    dev_mode = true
+    puts "Dev mode 2 has been activated."
+    name = "Chris"
+    pcorigin = "Skyrim"
+    pcclass = "Warrior"
+    @text_delay = 0.2
 end
+ 
 
 #takes name from user input, assigns to variable, capitalizes it
 if name == "Josie" || name == "Jos" || name == "Sicilian Baby" || name == "Josephine"
@@ -112,36 +168,33 @@ elsif name == "Emmy" || name == "Emilia" || name == "Em"
   puts "Hello my beautiful child! Thanks for testing this game for me! :) Love you!"
 end
 
-dialogue_pause(text_delay)
+dialogue_pause(@text_delay)
 puts "#{name}, you are a prisoner on an Imperial prison ship, sailing somewhere along the outskirts of Tamriel."
-dialogue_pause(text_delay)
+dialogue_pause(@text_delay)
 puts "You are unsure how you ended up here, as you have no recollection of the past few days. Even memories from prior are currently a hazy blur."
-dialogue_pause(text_delay)
+dialogue_pause(@text_delay)
 puts "As you struggle to recall what led you to this moment, you hear a booming voice from the cell next to yours."
-dialogue_pause(text_delay)
+dialogue_pause(@text_delay)
 puts "'Hey, you there! The name's Rurik.....and you are?'"
-dialogue_pause(text_delay_slow)
+dialogue_pause(@text_delay_slow)
 puts "..."
-dialogue_pause(text_delay)
+dialogue_pause(@text_delay)
 puts "'Pleasure to meet ya', #{name}!'"
-dialogue_pause(text_delay)
+dialogue_pause(@text_delay)
 puts "'Whereabouts are you from then?'"
 
 #takes player origin from user input, assigns to variable, capitalizes it
 pcorigin = gets.chomp.capitalize.strip
 
   if pcorigin.downcase == "skyrim"
-    puts
-    sleep(text_delay)
+    dialogue_pause(@text_delay)
     puts "'Aha! A fellow Skyrim native! Hopefully we can talk about the homeland over a nice mug of spiced mead one day. If we live through this gods-forsaken mess, that is.'"
   else
-    puts
-    sleep(text_delay)
+    dialogue_pause(@text_delay)
     puts "'Ah, from  #{pcorigin} eh? I've never been there myself, but I've heard grand tales from my seafaring friends. I hope we live long enough to share them with ya'"
   end 
 
-  puts
-  sleep(text_delay)
+  dialogue_pause(@text_delay)
   puts "'Say, what class are you? I'm a bit of a thief myself, though I bet I look the fool considering I got myself locked up here. I'm not the most quiet person unfortunately, try as I might...'"
 
   #takes player class from user input, assigns to variable
@@ -149,67 +202,53 @@ pcorigin = gets.chomp.capitalize.strip
 
 #this if/else statement gives different responses from Rurik depending on your class
 if pcclass.downcase == "stealth archer" || pcclass.downcase == "thief" || pcclass.downcase == "assassin" || pcclass.downcase == "rogue"
-  puts
-  sleep(text_delay)
+  dialogue_pause(@text_delay)
   puts "'Wow, a #{pcclass.downcase}? Great minds think alike, sneaky!'"
 elsif pcclass.downcase == "archer"
-  puts
-  sleep(text_delay)
+  dialogue_pause(@text_delay)
   puts "'Huh, an #{pcclass.downcase}? I think some precision fire will be very useful in our immediate future..'"
 elsif pcclass.downcase == "warrior" || pcclass.downcase == "fighter" || pcclass.downcase == "knight"
-  puts
-  sleep(text_delay)
+  dialogue_pause(@text_delay)
   puts "'A #{pcclass.downcase}, eh? Strong type, I bet. Just the sort we need to get out of this mess.'"
 elsif pcclass.downcase == "mage" || pcclass.downcase == "sorcerer" || pcclass.downcase == "wizard"
-  puts
-  sleep(text_delay)
+  dialogue_pause(@text_delay)
   puts "'Oh...a #{pcclass.downcase}? ....Well, I suppose brains over brawn has its merits in a situation like this. Just don't go zapping me accidentally, you hear?'"
 elsif pcclass.downcase == "no class" || pcclass.downcase == "classless"
-  puts
-  sleep(text_delay)
+  dialogue_pause(@text_delay)
   puts "#{pcclass.upcase} eh? No problem, you'll figure it out as we go."
 else
-  puts
-  sleep(text_delay)
+  dialogue_pause(@text_delay)
   puts "'A #{pcclass.downcase}? That's a new one for me. Well, whatever you are, I hope it serves us well. We need to get off this blasted ship..'"
 end 
   
-  sleep(text_delay)
-  puts
+  dialogue_pause(@text_delay)
   puts "Suddenly, the ship lurches violently, and you are thrown to the wall of your cell. A rush of water and debris suddenly gushes into the hallway with tremendous force. "
-  puts
-  sleep(text_delay_slow)
+  dialogue_pause(@text_delay)
   puts "'Woah #{name}, you alright?' You open your eyes and see a tall, dark-haired Nord standing before you with an outstretched hand. 
-'Looks like the walls of our cells were broken, and I think I can use this metal piece to pry the cell bars apart.'"
-  puts
-  sleep(text_delay)
+  'Looks like the walls of our cells were broken, and I think I can use this metal piece to pry the cell bars apart.'"
+  dialogue_pause(@text_delay) 
   puts "In an incredible feat of strength, Rurik wedges the metal piece into the iron bars and pries them apart."
-  puts
-  sleep(text_delay)
+  dialogue_pause(@text_delay)
   puts "'This should be enough to squeeze out. You lead the way #{name}, we need to make it to the captain's quarters. It'll be safer there.'"
 
   #this if/else statement looks at the player class variable and has Rurik give the PC a different weapon or item depending on the class. Classic TES! 
   #In the future this could become the spot where a combat tutorial occurs
   #The item that Rurik gives the player should be an actual object stored in inventory with damage or restoration stats eventually. 
   if pcclass.downcase == "stealth archer" || pcclass.downcase == "thief" || pcclass.downcase == "assassin" || pcclass.downcase == "rogue"
-    puts
-    sleep(text_delay)
+    dialogue_pause(@text_delay)
     puts "After you and Rurik squeeze out of the cell, he picks up a rusty iron dagger from the ground and hands it to you. 'This should serve, for now at least. Let's get out of here!'"
   elsif pcclass.downcase == "archer" || pcclass.downcase == "ranger"
-    puts
-    sleep(text_delay)
+    dialogue_pause(@text_delay)
     puts "After you and Rurik squeeze out of the cell, he picks up a water-logged wooden bow and a few arrows from the ground and hands it to you. 'This should serve, for now at least. 
 Keep an eye out for some arrows. Let's get out of here!'"
   elsif pcclass.downcase == "warrior" || pcclass.downcase == "fighter" || pcclass.downcase == "knight"
-    puts
-    sleep(text_delay)
+    dialogue_pause(@text_delay)
     puts "After you and Rurik squeeze out of the cell, he picks up a rusty iron mace from the ground and hands it to you. 'This should serve, for now at least. Let's get out of here!'"
   elsif pcclass.downcase == "mage" || pcclass.downcase == "sorcerer" || pcclass.downcase == "wizard"
-    puts
-    sleep(text_delay)
+    dialogue_pause(@text_delay)
     puts "After you and Rurik squeeze out of the cell, he picks up a splintering magical staff from a nearby floating shelf and hands it to you. 'This should serve for now. Let's get out of here!'"
   else 
-    sleep(text_delay)
+    dialogue_pause(@text_delay)
     puts "After you and Rurik squeeze out of the cell, he picks up an iron sword. 'This should serve for now. Let's get out of here!'"
   end 
 
@@ -220,53 +259,5 @@ Keep an eye out for some arrows. Let's get out of here!'"
   @charXP = 0
 
   #main menu with loop
-  loop do
-    
-    puts "
-    Main Menu
-      1. Woodcutting
-      2. Placeholder
-      3. Exit
-    "
-    selection = gets.chomp.strip
-  
-    if selection == "1" || selection == "1."
-      puts "Press Enter to return to the main menu"
-      
-      puts "You begin to chop a tree"
-      loop do
-        puts "Chop chop chop.."
-        @woodcuttingXP += 2 
-        @charXP += 5 
-        @wood += 3
-        #woodcutting level up system
-        if @woodcuttingXP >= 100
-          @woodcuttingLevel += 1
-          puts "You have reached level #{@woodcuttingLevel}!"
-          @woodcuttingXP = 0
-        end
-        #character level up system
-        if @charXP >= 1000
-          @charLevel += 1
-          puts "You have reached level #{@charLevel}!"
-          @charXP = 0
-        end
+  main_menu
 
-        sleep(1)
-        if IO.select([STDIN], nil, nil, 0)
-          STDIN.gets
-          puts "You have #{@wood} pieces of wood"
-          puts "You have achieved level #{@woodcuttingLevel} woodcutting"
-          puts "You have #{@charXP} experience points"
-          break
-        end
-      end 
-    elsif selection == "2" || selection == "2."
-        puts "Placeholder"
-    elsif selection == "3" || selection == "3."
-        puts "Goodbye!"
-        dialogue_pause(text_delay)
-        exit
-    else puts "Please input a numerical menu option."
-    end
-end
